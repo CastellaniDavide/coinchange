fcd2b7bbbbf1853e0e6e7b89d0b10b4ea2677e59
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-12-27 10:23:39 +0100

    2020-12-27

--- bin/coinchange.cpp
+++ bin/coinchange.cpp
@@ -1,42 +1,50 @@
-/**
- * @file coinchange.cpp
- *
- * @version 01.01 20201227
- *
- * @brief OIS 2020-12-15
- *
- * @ingroup coinchange
- * (Note: this needs exactly one @defgroup somewhere)
- *
- * @author Castellani Davide
- *
- * Contact: contacts@castellanidavide.it
- *
+/*
+ * NOTE: it is recommended to use this even if you don't
+ * understand the following code.
  */
 
-// Includes
-#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+
 using namespace std;
 
-// Variabiles
-int N;
+// input data
+long long V[15];
 
-// Main code
-int main()
-{
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+int main() {
+//  uncomment the following lines if you want to read/write from files
+//  ifstream cin("input.txt");
+//  ofstream cout("output.txt");
+// 100 10 50 10 0 13 11 10 1 10 100 1000 200 1 1
 
-  // Input
-  cin >> N;
 
-  // Code
-  // ...
+    int coin_values[15] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000};
+    long long sol[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    long long total = 0;
 
-  // Output
-  cout << N << endl;
+    for (int i = 0; i < 15; i++)
+    {
+        cin >> V[i];
+        total += V[i] * coin_values[i];
+    }
+    //cout << total << endl;
 
-  // End
-  return 0;
+    for (int i = 14; i >= 0; --i)
+    {
+        sol[i] = total / coin_values[i];
+        total -= sol[i] * coin_values[i];
+        //cout << total << endl;
+    }
+    total = 0;
+
+    for (int i = 0; i < 15; i++)
+    {
+        total += sol[i] * coin_values[i];
+    }
+    //cout << total << endl;
+
+    for (int i = 0; i < 15; i++)
+        cout << sol[i] << " ";
+    cout << endl;
+    return 0;
 }
